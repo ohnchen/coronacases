7bdcca291f70589b14d0a9ed05d6b974e3538e74
ohnchen <ohmejona@gmail.com>
2020-08-14 02:55:11 +0200

    adding comments

--- webscraper_corona.py
+++ webscraper_corona.py
@@ -2,9 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 import re 
 
-
+### setting the url to scrape from
 url = "https://www.worldometers.info/coronavirus/country/germany/"
+### getting content of this page ---> url
 r = requests.get(url)
+### Parsing content with BeautifulSoup
 soup = BeautifulSoup(r.text, "html.parser")
 
 approximate_search = soup.findAll(class_="maincounter-number")
