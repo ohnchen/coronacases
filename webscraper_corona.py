5076143ac12e9f8ba6303ea236da770fa31217ab
ohnchen <ohmejona@gmail.com>
2020-08-14 02:50:59 +0200

    add code

--- webscraper_corona.py
+++ webscraper_corona.py
@@ -9,8 +9,7 @@ soup = BeautifulSoup(r.text, "html.parser")
 
 approximate_search = soup.findAll(class_="maincounter-number")
 exact_search = re.findall(r">(.*?)<", str(approximate_search))
-conv = str(exact_search).replace(",", "")
 pattern = "\d"
 
-num = re.findall(pattern, conv)
+num = re.findall(pattern, str(exact_search))
 print("There have been {} Coronacases in Germany.".format("".join(num[0:6])))
