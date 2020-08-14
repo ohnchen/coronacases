c60b63fd1d1720bb6069f9956e197bc806f24f3d
ohnchen <ohmejona@gmail.com>
2020-08-14 02:49:43 +0200

    add code

--- webscraper_corona.py
+++ webscraper_corona.py
@@ -1,1 +1,16 @@
 import requests
+from bs4 import BeautifulSoup
+import re 
+
+
+url = "https://www.worldometers.info/coronavirus/country/germany/"
+r = requests.get(url)
+soup = BeautifulSoup(r.text, "html.parser")
+
+approximate_search = soup.findAll(class_="maincounter-number")
+exact_search = re.findall(r">(.*?)<", str(approximate_search))
+conv = str(exact_search).replace(",", "")
+pattern = "\d"
+
+num = re.findall(pattern, conv)
+print("There have been {} Coronacases in Germany.".format("".join(num[0:6])))
